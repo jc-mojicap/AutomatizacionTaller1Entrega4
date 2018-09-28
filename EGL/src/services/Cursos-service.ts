eb03726de1ad31e534869d2716403f655b6108c7
ja.martinezl <ja.martinezl@uniandes.edu.co>
2018-09-27 23:33:17 -0500

    Generación completa de archivos dinamicos aplicacion IONIC

--- EGL/src/services/Cursos-service.ts
+++ EGL/src/services/Cursos-service.ts
@@ -1,80 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Injectable } from '@angular/core';
+  
+import { CursosModel } from '../model/cursos/cursos';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+@Injectable()
+export class CursosService {
 
-//Estudiante
-import {EstudianteListPage} from '../pages/Estudiante/list/Estudiante.list';
-import {EstudianteEditPage} from '../pages/Estudiante/edit/Estudiante.edit';
-import {EstudianteAddPage} from '../pages/Estudiante/add/Estudiante.add';
-import {EstudianteService} from '../pages/Estudiante/services/Estudiante-service';
-//Profesor
-import {ProfesorListPage} from '../pages/Profesor/list/Profesor.list';
-import {ProfesorEditPage} from '../pages/Profesor/edit/Profesor.edit';
-import {ProfesorAddPage} from '../pages/Profesor/add/Profesor.add';
-import {ProfesorService} from '../pages/Profesor/services/Profesor-service';
-//Cursos
-import {CursosListPage} from '../pages/Cursos/list/Cursos.list';
-import {CursosEditPage} from '../pages/Cursos/edit/Cursos.edit';
-import {CursosAddPage} from '../pages/Cursos/add/Cursos.add';
-import {CursosService} from '../pages/Cursos/services/Cursos-service';
-//Direccion
-import {DireccionListPage} from '../pages/Direccion/list/Direccion.list';
-import {DireccionEditPage} from '../pages/Direccion/edit/Direccion.edit';
-import {DireccionAddPage} from '../pages/Direccion/add/Direccion.add';
-import {DireccionService} from '../pages/Direccion/services/Direccion-service';
+  cursos : CursosModel[];
+  constructor() {
+    this.cursos = []; 
+  }
 
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-  	  EstudianteListPage,
- 	  EstudianteEditPage,
- 	  EstudianteAddPage, 	  
-  	  ProfesorListPage,
- 	  ProfesorEditPage,
- 	  ProfesorAddPage, 	  
-  	  CursosListPage,
- 	  CursosEditPage,
- 	  CursosAddPage, 	  
-  	  DireccionListPage,
- 	  DireccionEditPage,
- 	  DireccionAddPage, 	  
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-  	  EstudianteListPage,
- 	  EstudianteEditPage,
- 	  EstudianteAddPage, 	  
-  	  ProfesorListPage,
- 	  ProfesorEditPage,
- 	  ProfesorAddPage, 	  
-  	  CursosListPage,
- 	  CursosEditPage,
- 	  CursosAddPage, 	  
-  	  DireccionListPage,
- 	  DireccionEditPage,
- 	  DireccionAddPage, 	  
-   
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-  	  EstudianteService, 	  
-  	  ProfesorService, 	  
-  	  CursosService, 	  
-  	  DireccionService, 	  
-  ]
-})
-export class AppModule {}
+  add(cursos:CursosModel){
+    this.cursos.push(cursos);
+  }
+
+  remove(cursos:CursosModel){
+    for(var i = 0; i < this.cursoss.length; i++) {
+        if(this.cursos[i] == cursos){
+            this.cursos.splice(i, 1);
+        }
+    }
+  }
+}
--- EGL/src/services/Profesor-service.ts
+++ EGL/src/services/Profesor-service.ts
@@ -1,80 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Injectable } from '@angular/core';
+  
+import { ProfesorModel } from '../model/profesores/profesores';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+@Injectable()
+export class ProfesorService {
 
-//Estudiante
-import {EstudianteListPage} from '../pages/Estudiante/list/Estudiante.list';
-import {EstudianteEditPage} from '../pages/Estudiante/edit/Estudiante.edit';
-import {EstudianteAddPage} from '../pages/Estudiante/add/Estudiante.add';
-import {EstudianteService} from '../pages/Estudiante/services/Estudiante-service';
-//Profesor
-import {ProfesorListPage} from '../pages/Profesor/list/Profesor.list';
-import {ProfesorEditPage} from '../pages/Profesor/edit/Profesor.edit';
-import {ProfesorAddPage} from '../pages/Profesor/add/Profesor.add';
-import {ProfesorService} from '../pages/Profesor/services/Profesor-service';
-//Cursos
-import {CursosListPage} from '../pages/Cursos/list/Cursos.list';
-import {CursosEditPage} from '../pages/Cursos/edit/Cursos.edit';
-import {CursosAddPage} from '../pages/Cursos/add/Cursos.add';
-import {CursosService} from '../pages/Cursos/services/Cursos-service';
-//Direccion
-import {DireccionListPage} from '../pages/Direccion/list/Direccion.list';
-import {DireccionEditPage} from '../pages/Direccion/edit/Direccion.edit';
-import {DireccionAddPage} from '../pages/Direccion/add/Direccion.add';
-import {DireccionService} from '../pages/Direccion/services/Direccion-service';
+  profesores : ProfesorModel[];
+  constructor() {
+    this.profesores = []; 
+  }
 
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-  	  EstudianteListPage,
- 	  EstudianteEditPage,
- 	  EstudianteAddPage, 	  
-  	  ProfesorListPage,
- 	  ProfesorEditPage,
- 	  ProfesorAddPage, 	  
-  	  CursosListPage,
- 	  CursosEditPage,
- 	  CursosAddPage, 	  
-  	  DireccionListPage,
- 	  DireccionEditPage,
- 	  DireccionAddPage, 	  
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-  	  EstudianteListPage,
- 	  EstudianteEditPage,
- 	  EstudianteAddPage, 	  
-  	  ProfesorListPage,
- 	  ProfesorEditPage,
- 	  ProfesorAddPage, 	  
-  	  CursosListPage,
- 	  CursosEditPage,
- 	  CursosAddPage, 	  
-  	  DireccionListPage,
- 	  DireccionEditPage,
- 	  DireccionAddPage, 	  
-   
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-  	  EstudianteService, 	  
-  	  ProfesorService, 	  
-  	  CursosService, 	  
-  	  DireccionService, 	  
-  ]
-})
-export class AppModule {}
+  add(profesores:ProfesorModel){
+    this.profesores.push(profesores);
+  }
+
+  remove(profesores:ProfesorModel){
+    for(var i = 0; i < this.profesoress.length; i++) {
+        if(this.profesores[i] == profesores){
+            this.profesores.splice(i, 1);
+        }
+    }
+  }
+}
